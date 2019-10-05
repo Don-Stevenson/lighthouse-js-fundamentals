9d9487958443602cfc30e7609d63f594525f345e
Don-Stevenson <dsteven1@gmail.com>
2019-10-05 18:44:56 +0000

    Using a function to check if a number is odd

---
+++ odd.js
@@ -0,0 +1,15 @@
+// const isEven  = function (num) {
+//   return num % 2 === 0;
+// }
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+//
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+const isOdd  = function (num) {
+  return num % 2 != 0;
+}
+
+console.log(isOdd(10));
+console.log(isOdd(11));
