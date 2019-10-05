98d066394da16b07583b8ebf3e60d35399133618
Don-Stevenson <dsteven1@gmail.com>
2019-10-05 18:39:12 +0000

    Using a function to evaluate even or odd

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-const sayHello  = function () {
-  console.log("Hello, world");
-}
-
-sayHello();
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+//
+// sayHello();
 
 const sayHello  = function (name) {
   console.log("Hello, " + name);
@@ -10,3 +10,20 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
